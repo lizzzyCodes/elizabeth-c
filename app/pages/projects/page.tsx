f971735893066ec689607c9790366af8cac53d5f
Elizabeth <ecastillooo87@gmail.com>
2024-05-15 00:00:11 -0500

    adding images

--- app/pages/projects/page.tsx
+++ app/pages/projects/page.tsx
@@ -1,8 +1,9 @@
 // This Page contains all the project cards
 import Heading from "@/components/heading";
 import ProjectCard from "@/components/projectcard";
-import SampleImage from "@/app/src/img/Icons/xaci.jpeg";
-import { alabamaStack, mockHireStack, grooveSnapStack } from "@/lib/data";
+import AlabamaImage from "@/app/src/img/Icons/alabama.jpg";
+import MockHireImage from "@/app/src/img/Icons/mockHire.jpg";
+import { alabamaStack, mockHireStack } from "@/lib/data";
 import Navbar from "@/components/navbar";
 
 export default function Project() {
@@ -14,23 +15,16 @@ export default function Project() {
         <ProjectCard
           title="Plant Growth in Alabama"
           text="This project analyzes existing plant data in several counties of Alabama, and see if they are similar with earlier expectations of what should grow in that region. Why Alabama? Based on google search, Alabama has more plant species than any other state in the United States."
-          img={SampleImage.src}
+          img={AlabamaImage.src}
           data={alabamaStack}
           link="/pages/projects/projectFiles/alabama"
         />
         <ProjectCard
           title="Mock Hire"
           text="Developed initial stages of a free platform for individuals to practice technical and behavioral interview skills through mock sessions with peers."
-          img={SampleImage.src}
+          img={MockHireImage.src}
           data={mockHireStack}
           link="/pages/projects/projectFiles/mockHire"
-        />
-        <ProjectCard
-          title="GrooveSnap"
-          text="COMING SOON"
-          img={SampleImage.src}
-          data={grooveSnapStack}
-          link="/pages/projects/projectFiles/alabama"
         />
       </div>
     </div>
