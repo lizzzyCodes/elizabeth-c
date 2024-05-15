b5f8ecac0e7837b5cf76ccf0161fbf2ff3c0c071
Elizabeth <ecastillooo87@gmail.com>
2024-05-15 00:22:06 -0500

    removed footer and header

--- app/pages/resume/page.tsx
+++ app/pages/resume/page.tsx
@@ -1,20 +1,34 @@
-import Experience from "@/components/experience"
-import Navbar from "@/components/navbar"
-import Skills from "@/components/skills"
-import { ExperienceData, InternData, logos } from "@/lib/data"
-import Footer from "@/components/footer";
-
+import Experience from "@/components/experience";
+import Navbar from "@/components/navbar";
+import Skills from "@/components/skills";
+import { ExperienceData, InternData, logos } from "@/lib/data";
 
 // overall page for resume
 export default function Resume() {
-  return  (
-  <section className="w-full">
-    <Navbar link='/'/>
-    <Skills/>
-    <Experience title='paypal' subheading='software engineer, checkout' date='sept 2022 - jan 2024' data={ExperienceData} image={logos[0].photo.src}/>
-    <Experience title='paypal' subheading='software engineer, intern'  date='june 2021 - sept 2021' data={InternData} image={logos[0].photo.src}/>
-    <Experience title='education' subheading='b.a geographic information systems && data science @ university of washington' date='june 2022' image={logos[1].photo.src}/>
-    <Footer />
-  </section> 
-  )
+  return (
+    <section className="w-full">
+      <Navbar link="/" />
+      <Skills />
+      <Experience
+        title="paypal"
+        subheading="software engineer, checkout"
+        date="sept 2022 - jan 2024"
+        data={ExperienceData}
+        image={logos[0].photo.src}
+      />
+      <Experience
+        title="paypal"
+        subheading="software engineer, intern"
+        date="june 2021 - sept 2021"
+        data={InternData}
+        image={logos[0].photo.src}
+      />
+      <Experience
+        title="education"
+        subheading="b.a geographic information systems && data science @ university of washington"
+        date="june 2022"
+        image={logos[1].photo.src}
+      />
+    </section>
+  );
 }
--- components/experience.tsx
+++ components/experience.tsx
@@ -1,25 +1,53 @@
-import Image from "next/image"
+import Image from "next/image";
 
 // title is company, subheading is title, date, image data
-export default function Experience({ title, subheading, date, image, data}  : { title:string, subheading:string, date:string }) {
-return (
-
-<section about="Experience Container Border" className=" flex-shrink-0 border-2 border-[#71EC8B] rounded-[21px] m-6 font-press-start text-white "> 
-    <div about="Summary Bullet List" className="p-4">
-        <div about='SubHeading with Logo, Date and Title' className="flex relative pl-2 pt-4">
-        <Image className="pr-2 object-contain " src={image}  width={80} height={50} alt='logo'/>
-        <h1 className="text-4xl lg:text-5xl uppercase text-[#ED6DD3] pt-2 ">{title}</h1>
-        <h2 about="Date Top Right Corner" className="uppercase hidden lg:block absolute top-0 right-0 pt-10 pr-10">{date}</h2>
+export default function Experience({
+  title,
+  subheading,
+  date,
+  image,
+  data,
+}: {
+  title: string;
+  subheading: string;
+  date: string;
+}) {
+  return (
+    <section
+      about="Experience Container Border"
+      className=" flex-shrink-0 border-2 border-[#71EC8B] rounded-[21px] m-6 font-press-start text-white "
+    >
+      <div about="Summary Bullet List" className="p-4">
+        <div
+          about="SubHeading with Logo, Date and Title"
+          className="flex relative pl-2 pt-4"
+        >
+          <Image
+            className="pr-2 object-contain "
+            src={image}
+            width={70}
+            height={50}
+            alt="logo"
+          />
+          <h1 className="text-4xl lg:text-5xl uppercase text-[#ED6DD3] pt-2 ">
+            {title}
+          </h1>
+          <h2
+            about="Date Top Right Corner"
+            className="uppercase hidden lg:block absolute top-0 right-0 pt-10 pr-10"
+          >
+            {date}
+          </h2>
         </div>
         <p className="pt-5 pl-2 uppercase">{subheading}</p>
         <div about="Experience Heading, Subheading and Text" className="p-2">
-        <span about="list" className=" font-['montserrat']"> 
-            { data?.map(( experience ) => (
-            <li key={experience.id}> {experience.item} </li>
+          <span about="list" className=" font-['montserrat']">
+            {data?.map((experience) => (
+              <li key={experience.id}> {experience.item} </li>
             ))}
-        </span>
+          </span>
         </div>
-    </div>
-</section>
-  ) 
+      </div>
+    </section>
+  );
 }
